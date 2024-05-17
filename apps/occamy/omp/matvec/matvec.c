adbd582708aa5a21db07e20ae1bf66ee4df51e9f
Cyril Koenig <cyril27koenig@gmail.com>
2024-05-17 18:41:15 -0400

    snitch: 2d tiled matvec

--- apps/occamy/omp/matvec/matvec.c
+++ apps/occamy/omp/matvec/matvec.c
@@ -36,13 +36,14 @@ static inline void fence() { asm volatile("fence" ::: "memory"); }
 // TCDM holds 32768 float-32 elements.
 // We keep 32 KiB of TCDM for stacks
 #define MAX_ELEM (128-32) * 1024 / (sizeof(DTYPE))
+#define CORES 8
 #define VERIFY 1
 
-void matmul(DTYPE *xout_, uint32_t xout_p_, DTYPE *x_, uint32_t x_p_, DTYPE *w_, uint32_t w_p_, int n_, int d_) {
+int matmul(DTYPE *xout_, uint32_t xout_p_, DTYPE *x_, uint32_t x_p_, DTYPE *w_, uint32_t w_p_, int n_, int d_) {
 
     if (n_ * 17 > MAX_ELEM) {
         printf("Error : Size too large\n\r");
-        return;
+        return -1;
     }
 
     char toprint[128];
@@ -81,13 +82,13 @@ void matmul(DTYPE *xout_, uint32_t xout_p_, DTYPE *x_, uint32_t x_p_, DTYPE *w_,
         dm_memcpy_async((void*)w_row_l1[0], (const void*)w_p , n * 8 * sizeof(DTYPE));
 
         int it = 0;
-        uint32_t t0 = 0, ttot = 0;
+        uint32_t t0 = 0, tot_dma = 0;
 
         for (int I = 0; I < d; I += 8) {
             int rows_left = d - I;
             t0 = read_csr(mcycle);
             dm_wait();
-            ttot += read_csr(mcycle) - t0;
+            tot_dma += read_csr(mcycle) - t0;
             if (rows_left > 8)
                 dm_memcpy_async(w_row_l1[(it+1)%2], w_p + n * sizeof(DTYPE) * (I+8), n * MIN(rows_left - 8, 8) * sizeof(DTYPE));
 
@@ -107,13 +108,123 @@ void matmul(DTYPE *xout_, uint32_t xout_p_, DTYPE *x_, uint32_t x_p_, DTYPE *w_,
             it++;
         }
 
-        snrt_printf("dma -> %u (now:%u)\n\r", ttot, read_csr(mcycle));
+        //snrt_printf("dma -> %u (now:%u)\n\r", tot_dma, read_csr(mcycle));
 
 #endif
 
     omp_exit:;
     }
     hero_add_timestamp("enter_omp_end", __func__, 1);
+    return 0;
+}
+
+int matmul_large(DTYPE *xout_, uint32_t xout_p_, DTYPE *x_, uint32_t x_p_, DTYPE *w_, uint32_t w_p_, int n_, int d_) {
+
+    char toprint[128];
+    snprintf(toprint, 128, "enter_omp_matvec-%u", n_);
+    hero_add_timestamp(toprint,__func__,0);
+
+#pragma omp target device(1) map(to : n_, d_, xout_p_, x_p_, w_p_)
+    {
+        volatile uint32_t n = n_;
+        volatile uint32_t d = d_;
+        volatile uint32_t xout_p = xout_p_;
+        volatile uint32_t x_p = x_p_;
+        volatile uint32_t w_p = w_p_;
+
+        uint32_t tiling = 1;
+        uint32_t smol_n;
+        while ( (n_/tiling) * (2*CORES + 2) > MAX_ELEM )
+            tiling = tiling << 1;
+
+        smol_n = n_/tiling;
+
+#ifdef __HERO_1
+
+        if (!n || !d || !xout_p || !x_p || !w_p || !tiling) {
+            goto omp_exit;
+        }
+
+        // printf("%x - %x %x %x %x %x\n\r", (uint32_t)n, (uint32_t)d, (uint32_t)xout_p, (uint32_t)x_p, (uint32_t)w_p);
+
+        DTYPE *x_l1[2];
+        x_l1[0] = (DTYPE *) snrt_l1alloc(smol_n * sizeof(DTYPE));
+        x_l1[1] = (DTYPE *) snrt_l1alloc(smol_n * sizeof(DTYPE));
+        DTYPE *w_row_l1[2];
+        w_row_l1[0] = (DTYPE *) snrt_l1alloc(CORES * smol_n * sizeof(DTYPE));
+        w_row_l1[1] = (DTYPE *) snrt_l1alloc(CORES * smol_n * sizeof(DTYPE));
+        DTYPE *res;
+        res = (__device DTYPE *) snrt_l1alloc(CORES * sizeof(DTYPE));
+
+        int it = 0;
+        uint32_t t0 = 0, tot_dma = 0, tot_dotp = 0;
+
+        dm_memcpy2d_async(w_row_l1[0], w_p , smol_n*sizeof(DTYPE), smol_n*sizeof(DTYPE), n*sizeof(DTYPE), CORES, 0);
+        dm_memcpy_async(x_l1[0],     x_p , smol_n*sizeof(DTYPE));
+
+        //printf("%i (%i %i) %i %i/%i (%i %i)\n\r", d, n, tiling, CORES, CORES/tiling, d, smol_n);
+
+        for (int I = 0; I < d; I += CORES) {
+            int rows_left = d - I;
+
+            for (int J = 0; J < n; J += smol_n) {
+                //printf("%i (%i->%i) (%i->%i)\n\r", I, I+CORES, J, J+smol_n);      
+                //t0 = read_csr(mcycle);
+                //dma_wait_cycles += read_csr(mcycle) - t0;
+
+                t0 = read_csr(mcycle);
+                snrt_dma_wait_all();
+                tot_dma += read_csr(mcycle) - t0;
+
+                //for(int i = 0; i < CORES * smol_n; i++) {
+                //    if(i % smol_n == 0)
+                //        printf("\n\r");
+                //    printf("(%i) %f ", w_row_l1[(it)%2][i]);
+                //}
+                //printf("\n\r");
+                //for(int i = 0; i < smol_n; i++) {
+                //    printf("(%i) %f ", x_l1[(it)%2][i]);
+                //}
+                //printf("\n\r");
+
+                if(J+smol_n >= n) {
+                    dm_memcpy2d_async(w_row_l1[(it+1)%2], w_p + (n * I + n * CORES)*sizeof(DTYPE), smol_n*sizeof(DTYPE), smol_n*sizeof(DTYPE), n*sizeof(DTYPE), CORES);
+                    dm_memcpy_async(x_l1[(it+1)%2], x_p, smol_n * sizeof(DTYPE));
+                } else {
+                    dm_memcpy2d_async(w_row_l1[(it+1)%2], w_p + (n * I + (J+smol_n))*sizeof(DTYPE), smol_n*sizeof(DTYPE), smol_n*sizeof(DTYPE), n*sizeof(DTYPE), CORES);
+                    dm_memcpy_async(x_l1[(it+1)%2],     x_p + (J+smol_n)*sizeof(DTYPE)      , smol_n * sizeof(DTYPE));
+                }
+
+                DTYPE* x_l1_it = (__device DTYPE*)x_l1[it%2];
+                DTYPE* w_row_l1_it = (__device DTYPE*)w_row_l1[it%2]; 
+                t0 = read_csr(mcycle);
+                #pragma omp parallel for
+                for (int i = 0; i < CORES; i++) {
+                    float acc;
+                    if (I + i >= d)
+                        goto end;
+                    fdotp_simd32b(x_l1[0], &(w_row_l1[it%2][snrt_cluster_core_idx() * n]), &acc, n);
+                    if(J)
+                        res[i] += acc;
+                    else
+                        res[i] = acc;
+                end:;
+                }
+                tot_dotp += read_csr(mcycle) - t0;
+                //printf("(%i) %f %f\n\r\n\r", res[0], res[1]);
+                it++;
+            }
+            dm_memcpy_async(xout_p + I * sizeof(DTYPE), res, CORES * sizeof(DTYPE));
+
+        }
+        //snrt_printf("dma -> %u (now:%u)\n\r", ttot, read_csr(mcycle));
+
+#endif
+
+    omp_exit:;
+    }
+    hero_add_timestamp("enter_omp_end", __func__, 1);
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -124,6 +235,7 @@ int main(int argc, char *argv[]) {
     DTYPE *C = NULL, *D = NULL, *E;
     // Verification matrices
     DTYPE *C_test = NULL, *D_test = NULL, *E_test;
+    int ret;
 
     int height = 20;
     int width = height;
@@ -147,24 +259,28 @@ int main(int argc, char *argv[]) {
     D_test = malloc(width * sizeof(DTYPE));
     E_test = malloc(height * sizeof(DTYPE));
 
+
     // Prepare data
     for (int i = 0; i < height; i++)
         for (int j = 0; j < width; j++)
-            C[i * width + j] = (DTYPE)((i + j)%4);
+            C[i * width + j] = (DTYPE)((i * width + j));
     for (int j = 0; j < width; j++)
-        D[j] = (DTYPE)(j%4);
+        D[j] = (j == 0) ? (DTYPE)(1) : (DTYPE)(0);
 
     for (int i = 0; i < height * width; i++)
         C_test[i] = C[i];
     for (int i = 0; i < width; i++)
         D_test[i] = D[i];
+    
+    asm volatile ("fence");
 
     // Offload !
-    matmul(E, E_phys, D, D_phys, C, C_phys, width, height);
-
+    ret = matmul(E, E_phys, D, D_phys, C, C_phys, width, height);
+    if(ret)
+        ret = matmul_large(E, E_phys, D, D_phys, C, C_phys, width, height);
 
 #ifndef __HERO_1
-#ifdef VERIFY
+
     // Execution on host
     char toprint[128];
     snprintf(toprint, 128, "enter_omp_verify_matvec-%u", width);
@@ -182,10 +298,10 @@ int main(int argc, char *argv[]) {
 
     // Verify result
     for (int i = 0; i < height; i++) {
-            if(E_test[i] != E[i])
-                printf("nope %i\n\r", i);
+        if(E_test[i] != E[i])
+            printf("nope %i\n\r", i);
     }
-#endif
+
 
     // Print all the recorded timestamps
     hero_print_timestamp();
