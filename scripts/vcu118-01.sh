232a45141374088a808f162705d2cddb9d001cac
Cyril Koenig <cyril27koenig@gmail.com>
2024-07-10 12:25:38 +0200

    scripts: Add vcu118 script

---
+++ scripts/vcu118-01.sh
@@ -0,0 +1,5 @@
+export XILINX_PORT=3235
+export XILINX_FPGA_PATH=xilinx_tcf/Digilent/210308A3B1D8
+export XILINX_HOST=bordcomputer
+export XILINX_MAC_ADDR="[ 00 0a 35 04 e1 60 ]"
+export XILINX_IP=172.31.182.93
