bad45fb43d5882c9a6d7fdd23346a944e04be30b
Cyril Koenig <cyril27koenig@gmail.com>
2024-07-22 10:34:28 +0200

    scripts: Correct setenv.sh on clean repository

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -19,8 +19,10 @@ export RISCV=$CVA6_SDK/buildroot/output/host/
 export RV64_SYSROOT=$RISCV/riscv64-buildroot-linux-gnu/sysroot
 export CROSS_COMPILE=$RISCV/bin/riscv64-buildroot-linux-gnu-
 # Parse the device Linux path from buildroot
+if [ -f $CVA6_SDK/buildroot/Makefile ]; then
 export BR_LINUX_DIR=$CVA6_SDK/buildroot/output/$(make -C cva6-sdk/buildroot linux-show-info \
                     | python3 -c "import sys, json; print(json.loads(sys.stdin.readlines()[1])['linux']['build_dir'])")
+fi
 # For Occamy bootrom
 export UBOOT_SPL_BIN=$CVA6_SDK/install64/u-boot-spl.bin
 export UBOOT_ITB=$CVA6_SDK/install64/u-boot.itb
